bbf47cc3ef0f48bf4a1cc2bae912365b0a8bd0de
Walter Boyce <walter.boyce@shardimage.com>
2019-03-13 14:55:43 +0100

    Additional ServiceUnavailableHttpException functions.

--- src/web/exceptions/ServiceUnavailableHttpException.php
+++ src/web/exceptions/ServiceUnavailableHttpException.php
@@ -14,8 +14,36 @@ namespace shardimage\shardimagephpapi\web\exceptions;
  */
 class ServiceUnavailableHttpException extends HttpException
 {
+
+    /**
+     * {@inheritdoc}
+     */
     public function __construct($message = null, $code = 0, $errors = null, $previous = null)
     {
         parent::__construct($message, $code, $errors, $previous, 503);
+    }
+
+    /**
+     * Get Retry-After header value in seconds
+     * @return int|string|null
+     */
+    public function getRetryAfter()
+    {
+        $headers = $this->getHeaders();
+        $retryAfter = isset($headers['retry-after']) ? $headers['retry-after'] : null;
+        return is_numeric($retryAfter) ? (int)$retryAfter : $retryAfter;
+    }
+
+    /**
+     * Get absolute timestamp how long the user agent should wait before making a follow-up request
+     * @return int
+     */
+    public function getRetryAfterAbsolute()
+    {
+        $retryAfter = $this->getRetryAfter();
+        if (is_string($retryAfter)) {
+            return strtotime($retryAfter);
+        }
+        return time() + $retryAfter;
     }
 }
